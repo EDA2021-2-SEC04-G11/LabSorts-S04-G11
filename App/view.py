016d5b9d96c5775a4e09a29a5413db3cbfe0e203
Ronald Yesid Diaz Pardo <r.diazp@uniandes.edu.co>
2021-09-10 12:03:38 -0500

    printSortResults view.py

--- App/view.py
+++ App/view.py
@@ -26,6 +26,8 @@ import controller
 from DISClib.ADT import list as lt
 assert cf
 
+default_limit = 1000
+sys.setrecursionlimit(default_limit*10)
 
 """
 La vista se encarga de la interacción con el usuario
@@ -84,6 +86,17 @@ def printBestBooks(books):
 def printSortResults(ord_books, sample=10):
     # TODO completar modificaciones para el laboratorio 4
     pass
+
+def printSortResults(ord_books, sample=10):
+    size = lt.size(ord_books)
+    if size > sample:
+        print("Los primeros ", sample, " libros ordenados son:")
+        i=1
+        while i <= sample:
+            book = lt.getElement(ord_books,i)
+            print('Titulo: ' + book['title'] + ' ISBN: ' + book['isbn'] + ' Rating: ' + book['average_rating'])
+            i+=1
+
 
 catalog = None
 
